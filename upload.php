dbadc0d40ec98dd289abd5867f8bea3ebab3bf04
remusnegrota <remusnegrota@gmail.com>
2014-03-13 18:03:02 +0200

    Added projected to repository

---
+++ upload.php
@@ -0,0 +1,18 @@
+<?php
+
+if(!is_dir("recordings")){
+	$res = mkdir("recordings",0777); 
+}
+
+// pull the raw binary data from the POST array
+$data = substr($_POST['data'], strpos($_POST['data'], ",") + 1);
+// decode it
+$decodedData = base64_decode($data);
+// print out the raw data, 
+//echo ($decodedData);
+$filename = urldecode($_POST['fname']);
+// write the data out to the file
+$fp = fopen('recordings/'.$filename, 'wb');
+fwrite($fp, $decodedData);
+fclose($fp);
+?>
