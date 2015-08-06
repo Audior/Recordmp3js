6a439b878465025741ba6147d21b605d65ed78f4
Simon Zalar <totalfix@gmail.com>
2015-08-06 22:34:37 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -10,7 +10,7 @@ $data = substr($_POST['data'], strpos($_POST['data'], ",") + 1);
 $decodedData = base64_decode($data);
 // print out the raw data, 
 //echo ($decodedData);
-$filename = 'audio_recording_' . date( 'Y-m-d-H-i-s' ) .'.php';
+$filename = 'audio_recording_' . date( 'Y-m-d-H-i-s' ) .'.mp3';
 
 // write the data out to the file
 $fp = fopen('recordings/'.$filename, 'wb');
